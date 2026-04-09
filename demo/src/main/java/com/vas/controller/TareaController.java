2013af971e01f1083abfd77d08b3166b0da15b63
josuelrj <rrodriguez50905@ufide.ac.cr>
2026-04-08 22:51:08 -0600

    Backend empleados y tareas - Josuel

--- demo/src/main/java/com/vas/controller/TareaController.java
+++ demo/src/main/java/com/vas/controller/TareaController.java
@@ -16,7 +16,25 @@ public class TareaController {
 
     @PostMapping
     public Map<String, String> agregar(@RequestBody Map<String, String> tarea) {
+        tarea.put("id", UUID.randomUUID().toString());
         tareas.add(tarea);
         return tarea;
+    }
+
+    @PutMapping("/{id}")
+    public Map<String, String> actualizar(@PathVariable String id, @RequestBody Map<String, String> nuevo) {
+        for (Map<String, String> t : tareas) {
+            if (t.get("id").equals(id)) {
+                t.putAll(nuevo);
+                return t;
+            }
+        }
+        return null;
+    }
+
+    @DeleteMapping("/{id}")
+    public String eliminar(@PathVariable String id) {
+        tareas.removeIf(t -> t.get("id").equals(id));
+        return "Tarea eliminada";
     }
 }
